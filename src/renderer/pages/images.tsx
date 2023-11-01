7981c6669f42d035449231366451dcce19c7ae70
albertpb <palumbo.alb@gmail.com>
2023-11-01 20:13:03 -0300

    add button to show rating and name in images page

--- src/renderer/pages/images.tsx
+++ src/renderer/pages/images.tsx
@@ -29,6 +29,7 @@ export default function Images() {
 
   const [zoomLevel, setZoomLevel] = useState<number>(3);
   const [showRating, setShowRating] = useState<boolean>(true);
+  const [showModelName, setShowModelName] = useState<boolean>(true);
   const [imageAnimated, setImageAnimated] = useState<boolean>(true);
   const [deleteActive, setDeleteActive] = useState<boolean>(false);
   const [sortBy, setSortBy] = useState<string>('ratingDesc');
@@ -340,9 +341,13 @@ export default function Images() {
               className="object-cover"
               draggable={false}
             />
-            <div className="absolute bottom-0 left-0 w-full p-3 flex flex-col">
-              <p className="text font-bold text-white">{item.model}</p>
-            </div>
+            {showModelName && (
+              <div className="absolute bottom-0 left-0 w-full p-3 flex flex-col">
+                <p className="text font-bold text-white truncate">
+                  {item.model}
+                </p>
+              </div>
+            )}
           </figure>
         </div>
       );
@@ -403,6 +408,57 @@ export default function Images() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                />
+              </svg>
+            </button>
+          </li>
+          <li
+            className="tooltip tooltip-right"
+            data-tip={`show model name: ${showModelName ? 'on' : 'off'}`}
+          >
+            <button
+              type="button"
+              onClick={() => setShowModelName(!showModelName)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={classNames([
+                  'w-5 h-5',
+                  { 'stroke-green-500': showRating },
+                ])}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 9h16.5m-16.5 6.75h16.5"
+                />
+              </svg>
+            </button>
+          </li>
+          <li
+            className="tooltip tooltip-right"
+            data-tip={`show rating: ${showRating ? 'on' : 'off'}`}
+          >
+            <button type="button" onClick={() => setShowRating(!showRating)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={classNames([
+                  'w-5 h-5',
+                  { 'fill-green-500': showRating },
+                ])}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
                 />
               </svg>
             </button>
