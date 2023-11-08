e41e0aa8ba17c5c82793bd5f425a4bcf469318cc
albertpb <palumbo.alb@gmail.com>
2023-11-08 13:03:23 -0300

    fix: prevent change rating with arrow keys

--- src/renderer/components/Rating.tsx
+++ src/renderer/components/Rating.tsx
@@ -12,6 +12,14 @@ export default function Rating({
   onClick?: (event: MouseEvent<HTMLInputElement>, value: number) => void;
   hidden?: boolean;
 }) {
+  const click = (e: MouseEvent<HTMLInputElement>, v: number) => {
+    const target = e.target as HTMLInputElement;
+    target.blur();
+    if (onClick) {
+      onClick(e, v);
+    }
+  };
+
   const stars = (
     <>
       <input
@@ -19,7 +27,7 @@ export default function Rating({
         name={`${id}_rating-2`}
         className="mask mask-star-2 bg-orange-400"
         checked={value === 1}
-        onClick={(e) => onClick && onClick(e, 1)}
+        onClick={(e) => click(e, 1)}
         onChange={() => {}}
       />
       <input
@@ -27,7 +35,7 @@ export default function Rating({
         name={`${id}_rating-2`}
         className="mask mask-star-2 bg-orange-400"
         checked={value === 2}
-        onClick={(e) => onClick && onClick(e, 2)}
+        onClick={(e) => click(e, 2)}
         onChange={() => {}}
       />
       <input
@@ -35,7 +43,7 @@ export default function Rating({
         name={`${id}_rating-2`}
         className="mask mask-star-2 bg-orange-400"
         checked={value === 3}
-        onClick={(e) => onClick && onClick(e, 3)}
+        onClick={(e) => click(e, 3)}
         onChange={() => {}}
       />
       <input
@@ -43,7 +51,7 @@ export default function Rating({
         name={`${id}_rating-2`}
         className="mask mask-star-2 bg-orange-400"
         checked={value === 4}
-        onClick={(e) => onClick && onClick(e, 4)}
+        onClick={(e) => click(e, 4)}
         onChange={() => {}}
       />
       <input
@@ -51,7 +59,7 @@ export default function Rating({
         name={`${id}_rating-2`}
         className="mask mask-star-2 bg-orange-400"
         checked={value === 5}
-        onClick={(e) => onClick && onClick(e, 5)}
+        onClick={(e) => click(e, 5)}
         onChange={() => {}}
       />
     </>
